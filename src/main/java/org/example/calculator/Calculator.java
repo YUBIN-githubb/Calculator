3a403b6ab4c9ef6cffa3eff734deb7fd80993220
Moon YuBin <yubin5744@gmail.com>
2025-01-03 11:08:08 +0900

    중간 수정, 인풋 숫자 형변환 구현 아직 안됨

--- src/main/java/org/example/calculator/Calculator.java
+++ src/main/java/org/example/calculator/Calculator.java
@@ -4,22 +4,43 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Write first number to calculate: ");
-        int firstNumber = sc.nextInt();
-        System.out.println("Write second number to calculate: ");
-        int secondNumber = sc.nextInt();
-        System.out.println("Write operator to calculate: ");
-        String operator = sc.next();
+        while(true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Write first number to calculate: ");
+            int firstNumber = sc.nextInt();
+            System.out.println("Write second number to calculate: ");
+            int secondNumber = sc.nextInt();
+            System.out.println("Write operator to calculate: ");
+            String operator = sc.next();
 
-        int result = 0;
-        if(operator.equals("+")){
-            result = firstNumber + secondNumber;
+            int result = 0;
+            if(operator.equals("+")){
+                result = firstNumber + secondNumber;
+            } else if (operator.equals("-")) {
+                result = firstNumber - secondNumber;
+            } else if (operator.equals("*")) {
+                result = firstNumber * secondNumber;
+            } else if (operator.equals("/")) {
+                if(secondNumber == 0){
+                    System.out.println("Division by zero is impossible");
+                    System.out.println("first number is: " + firstNumber);
+                    System.out.println("rewrite the second number: ");
+                    secondNumber = sc.nextInt();
+                }
+                result = firstNumber / secondNumber;
+            }
+
+            System.out.println("result is " + result);
+
+            System.out.println("If you wanna continue, Write continue. Unless write exit. Write answer: ");
+            String answer = sc.next();
+            if(answer.equals("exit")){
+                break;
+            } else if (answer.equals("continue")) {
+                continue;
+            }
+
         }
-
-        System.out.println(result);
-
-
 
 
     }
