d8b6d2a7bbf16f0eef74364f16168c8f2c86f6d4
Moon YuBin <yubin5744@gmail.com>
2025-01-09 21:56:28 +0900

    최종 수정

--- src/main/java/org/example/calculator/App.java
+++ src/main/java/org/example/calculator/App.java
@@ -9,48 +9,131 @@ public class App {
     public static void main(String[] args) {
         while(true) {
             Scanner sc = new Scanner(System.in);
-            System.out.println("Write first number to calculate: ");
-            int firstNumber = sc.nextInt();
 
-            if (firstNumber < 0) {
-                System.out.println("Please enter a positive number");
-                System.out.println("Write first number to calculate: ");
-                firstNumber = sc.nextInt();
+            //숫자 입력
+            int firstNumber;
+            System.out.println("계산할 첫번째 숫자을 입력하세요.(0을 포함한 양의 정수): ");
+            while (true) {
+                try {
+                    firstNumber = sc.nextInt();
+                    if (firstNumber < 0) {
+                        System.out.println("0을 포함한 양수를 입력하세요.");
+                        System.out.println("계산할 첫번째 숫자을 입력하세요.(0을 포함한 양의 정수): ");
+                        sc.nextLine();
+                        continue;
+                    }
+                    break;
+                } catch (Exception e) {
+                    System.out.println("정수를 입력하세요.");
+                    System.out.println("계산할 첫번째 숫자을 입력하세요.(0을 포함한 양의 정수): ");
+                    sc.nextLine();
+                }
             }
 
-            System.out.println("Write second number to calculate: ");
-            int secondNumber = sc.nextInt();
-
-            if (secondNumber < 0) {
-                System.out.println("Please enter a positive number");
-                System.out.println("Write second number to calculate: ");
-                secondNumber = sc.nextInt();
+            int secondNumber;
+            System.out.println("계산할 두번째 숫자을 입력하세요.(0을 포함한 양의 정수): ");
+            while (true) {
+                try {
+                    secondNumber = sc.nextInt();
+                    if (secondNumber < 0) {
+                        System.out.println("0을 포함한 양수를 입력하세요.");
+                        System.out.println("계산할 두번째 숫자을 입력하세요.(0을 포함한 양의 정수): ");
+                        sc.nextLine();
+                        continue;
+                    }
+                    break;
+                } catch (Exception e) {
+                    System.out.println("정수를 입력하세요.");
+                    System.out.println("계산할 두번째 숫자을 입력하세요.(0을 포함한 양의 정수): ");
+                    sc.nextLine();
+                }
             }
 
-            System.out.println("Write operator to calculate: ");
-            String operator = sc.next();
+            //연산자 입력
+            System.out.println("계산할 연산자를 입력하세요.: ");
+            char operator;
+            while (true) {
+                try {
+                    String inputOperator = sc.next();
+                    if (inputOperator.isEmpty()) {
+                        System.out.println("계산할 연산자를 입력하세요.: ");
+                        sc.nextLine();
+                        continue;
+                    }
+                    operator = inputOperator.charAt(0);
+                    if (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
+                        System.out.println("입력할 수 있는 연산자는 오직 +,-,*,/ 입니다.");
+                        System.out.println("계산할 연산자를 입력하세요.: ");
+                        sc.nextLine();
+                        continue;
+                    }
+                    break;
+                }
+                catch (Exception e) {
+                    System.out.println("유효하지 않은 입력입니다. 입력할 수 있는 연산자는 오직 +,-,*,/ 입니다.");
+                    System.out.println("계산할 연산자를 입력하세요.: ");
+                }
+            }
 
-
+            //계산 수행
             Calculator2 doCalculate = new Calculator2();
             doCalculate.calculate(firstNumber, secondNumber, operator);
-            doCalculate.fullGetter();
             doCalculate.printResult();
 
+            //계산 결과 기록
+            System.out.println("현재까지 기록된 계산결과입니다.");
+            doCalculate.fullGetter();
 
+            //계산 결과 삭제
+            System.out.println("계산결과를 삭제하고 싶다면 remove를 입력하세요.\n계산결과를 수정하고 싶다면 edit을 입력하세요.\n그렇지 않다면 아무 키나 입력하세요: " );
+            String removeAnswer = sc.next();
+            if(removeAnswer.equals("remove")) {
+                while(true) {
+                    System.out.println("현재까지 기록된 계산결과입니다.");
+                    doCalculate.fullGetter();
+                    if(doCalculate.getter().isEmpty()) {
+                        System.out.println("기록된 계산결과가 없습니다. 삭제를 종료합니다.");
+                        break;
+                    }
+                    System.out.println("가장 첫번째 결과부터 삭제됩니다.\n삭제를 원하면 1을 입력하세요. 삭제를 종료하고 싶다면 0을 입력하세요.: ");
+                    int removeInt = sc.nextInt();
+                    if(removeInt == 1) {
+                        doCalculate.removeResult();
+                    } else if (removeInt == 0) {
+                        break;
+                    }
+                }
 
+            //계산결과 수정
+            } else if (removeAnswer.equals("edit")) {
+                while(true) {
+                    System.out.println("현재까지 기록된 계산결과입니다.");
+                    doCalculate.fullGetter();
+                    System.out.println("수정하고 싶은 결과의 인덱스를 입력하세요.(0부터 시작): ");
+                    int editIndex = sc.nextInt();
+                    System.out.println("어떤 값으로 수정하시겠습니까? 값을 입력하세요.: ");
+                    int editValue = sc.nextInt();
+                    doCalculate.setter(editIndex, editValue);
+                    System.out.println("수정된 계산 결과입니다.");
+                    doCalculate.fullGetter();
+                    System.out.println("계속 수정을 원할 경우 1을 입력하세요. 수정을 종료하고 싶다면 0을 입력하세요.: ");
+                    int editInt = sc.nextInt();
+                    if(editInt == 1) {
+                        continue;
+                    } else if (editInt == 0) {
+                        break;
+                    }
+                }
+            }
 
-
-
-            System.out.println("If you wanna continue, Write continue. Unless write exit. Write answer: ");
+            //종료 여부 결정
+            System.out.println("계산을 이어서 수행하고 싶은 경우 continue 입력, 종료하고 싶은 경우 exit을 입력하세요.: ");
             String answer = sc.next();
             if(answer.equals("exit")){
                 break;
             } else if (answer.equals("continue")) {
                 continue;
             }
-
         }
-
-
     }
 }
--- src/main/java/org/example/calculator2/Calculator2.java
+++ src/main/java/org/example/calculator2/Calculator2.java
@@ -4,59 +4,51 @@ import java.util.*;
 
 public class Calculator2 {
 
-    //field
-    //use static keyword to share resultArray between instance
-    static private ArrayList<Integer> resultArray = new ArrayList<Integer>();
-    int result = 0;
+    //객체 간의 계산결과를 공유하기 위해서 static 키워드 사용
+    static private List<Integer> resultArray = new ArrayList<Integer>();
+    private int result = 0;
 
-    //method
-    public void calculate(int firstNumber, int secondNumber, String operator) {
+    //계산 메서드
+    public void calculate(int firstNumber, int secondNumber, char operator) {
         Scanner sc = new Scanner(System.in);
-        if(operator.equals("+")){
+        if(operator == '+'){
             this.result = firstNumber + secondNumber;
-        } else if (operator.equals("-")) {
+        } else if (operator == '-') {
             this.result = firstNumber - secondNumber;
-        } else if (operator.equals("*")) {
+        } else if (operator == '*') {
             this.result = firstNumber * secondNumber;
-        } else if (operator.equals("/")) {
+        } else if (operator == '/') {
             if(secondNumber == 0){
-                System.out.println("Division by zero is impossible");
-                System.out.println("first number is: " + firstNumber);
-                System.out.println("rewrite the second number: ");
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                System.out.println("첫번째 숫자: " + firstNumber);
+                System.out.println("두번째 숫자를 다시 입력하세요: ");
                 secondNumber = sc.nextInt();
             }
             this.result = firstNumber / secondNumber;
-
         }
-
         resultArray.add(this.result);
-
     };
-
-    public int getter(int index) {
-        return resultArray.get(index);
-    };
+    public List<Integer> getter() {
+        return resultArray;
+    }
 
     public void setter(int index, int value) {
         resultArray.set(index, value);
     };
 
     public void printResult() {
-        System.out.println("result is " + resultArray.get(resultArray.size()-1));
+        System.out.println("계산 결과는 " + resultArray.get(resultArray.size()-1) + "입니다.");
     }
 
     public void removeResult() {
-        resultArray.remove(resultArray.remove(0));
+        resultArray.remove(0);
     }
 
     public void fullGetter() {
         System.out.println(resultArray.toString());
     };
 
-
     public static void main(String[] args) {
-
-
 
     }
 }
